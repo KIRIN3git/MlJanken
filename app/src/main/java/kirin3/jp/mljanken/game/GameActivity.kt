8723090f799de7fd1077163a2d8fd562a3eacbb3
KIRIN3 <scomputer13@gmail.com>
2019-03-19 00:12:41 +0900

    基礎作成005
    画像準備

--- app/src/main/java/kirin3/jp/mljanken/game/GameActivity.kt
+++ app/src/main/java/kirin3/jp/mljanken/game/GameActivity.kt
@@ -1,7 +1,13 @@
 package kirin3.jp.mljanken.game
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.ImageView
+import com.airbnb.lottie.LottieAnimationView
 import kirin3.jp.mljanken.R
 import kirin3.jp.mljanken.util.LogUtils
 
@@ -16,9 +22,21 @@ class GameActivity : AppCompatActivity() {
 
         var sCname: String = LogUtils.makeLogTag(GameActivity::class.java);
 
+        val img_man_gu = findViewById(R.id.imgManGu) as ImageView
+        val img_man_choki = findViewById(R.id.imgManChoki) as ImageView
+        val img_man_pa = findViewById(R.id.imgManPa) as ImageView
+
+        choiceJanken(img_man_gu)
+        choiceJanken(img_man_choki)
+        choiceJanken(img_man_pa)
+
     }
 
-    fun xxx(){
-
+    fun choiceJanken(img:ImageView){
+        img.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                img.setBackgroundColor(ContextCompat.getColor(this@GameActivity, R.color.lightGreen))
+            }
+        })
     }
 }
